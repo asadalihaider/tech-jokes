eab9b2a436d45fcdf82b77b1db20ed80611e567e
Asad Ali Haider <108663729+asadalihaider@users.noreply.github.com>
2023-12-26 22:01:37 +0500

    Use pluginOptions for workboxOptions

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
@@ -11,22 +12,23 @@ module.exports = defineConfig({
     },
   },
   pwa: {
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      navigateFallback: '/index.html',
-      runtimeCaching: [
-        {
-          urlPattern: new RegExp(`^${process.env.VUE_APP_JOKE_API_URL}/.*\\?Lang=`),
-          handler: 'networkFirst',
-          options: {
-            networkTimeoutSeconds: 30,
-            cacheName: 'api-cache',
-            cacheableResponse: {
-              statuses: [0, 200],
+    pluginOptions: {
+      workboxOptions: {
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: new RegExp(`^${process.env.VUE_APP_JOKE_API_URL}/.*\\?Lang=`),
+            handler: 'networkFirst',
+            options: {
+              networkTimeoutSeconds: 30,
+              cacheName: 'api-cache',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
             },
           },
-        },
-      ],
+        ],
+      },
     },
   },
 });
